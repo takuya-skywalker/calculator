d63ddc0dfcc93ee0843edf63a1644ab2cc53db45
TaroEraAfua <t.era@afua.co.jp>
2023-01-31 11:31:40 +0900

    不要ファイル、ソースコードの削除

--- lib/main.dart
+++ lib/main.dart
@@ -2,16 +2,7 @@ import 'package:calculator2/view/pages/calculator_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: CalculatorPage(),
-    );
-  }
+  runApp(const MaterialApp(
+    home: CalculatorPage(),
+  ));
 }
